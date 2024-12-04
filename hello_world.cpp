8b4c0588598a8e9b8777096b221975139278d61b
xRafay <123715905+xRafay@users.noreply.github.com>
2024-12-04 13:54:20 +0500

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Include the iostream library for input and output
+
+int main() {
+    std::cout << "Hello World" << std::endl; // Print "Hello World" followed by a new line
+    return 0; // Return 0 to indicate that the program ended successfully
+}
